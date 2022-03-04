34529014877b2ade36de610c173da481dc751c99
RanjithSatla <ranjithsatla327@gmail.com>
2022-03-04 20:34:50 +0530

    Assign 1a Refactor

--- 1a.js
+++ 1a.js
@@ -1,18 +1,18 @@
-// 1.a. Write a function which count the number of occurrence of words in a paragraph or a sentence.The function `countWords` takes a paragraph and two words as parameters. It compare  which word is most frequently occurred in the paragraph.
-// ```js
+/*1.a. Write a function which count the number of occurrence of words in a paragraph or a sentence.The function `countWords` takes a paragraph and two words as parameters. It compare  which word is most frequently occurred in the paragraph.
+```js
 
-// const paragraph = 'I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.';
-// console.log(countWords(paragraph,'love', 'you'));
-// The word love more frequently occurred than you.
+const paragraph = 'I love teaching. If you do not love teaching what else can you love. I love JavaScript if you do not love something which can give life to your application what else can you love.';
+console.log(countWords(paragraph,'love', 'you'));
+The word love more frequently occurred than you.*/
 
 const wordCount = (para, w1, w2) => {
-  const res = para.split(" ");
+  const words = para.split(" ");
   let l = 0;
   let y = 0;
-  for (const w of res) {
-    if (w.includes(w1)) {
+  for (const word of words) {
+    if (word.includes(w1)) {
       l++;
-    } else if (w.includes(w2)) {
+    } else if (word.includes(w2)) {
       y++;
     }
   }
