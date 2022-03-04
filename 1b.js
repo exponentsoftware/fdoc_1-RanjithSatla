41a8b295b067013ba34e5c3b2022b65097300bc4
RanjithSatla <ranjithsatla327@gmail.com>
2022-03-04 21:10:05 +0530

    Clean Text

--- 1b.js
+++ 1b.js
@@ -3,16 +3,10 @@
 const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
     console.log(cleanText(sentence));*/
 
-const cleanText = (text) => {
-  nrmlText = text.split(" ");
-
-  for (const word of nrmlText) {
-    if (word.includes("%" || "$" || "@" || "&" || "#")) {
-      word.replace("%" || "$" || "@" || "&" || "#", "");
-      console.log(word);
-    }
-  }
-};
+function cleanText(sentence) {
+  return sentence.replace(/[^a-zA-Z. ]/g, "");
+}
 
 const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`;
+
 console.log(cleanText(sentence));
